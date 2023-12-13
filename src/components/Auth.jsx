f0695c49c51b6a663417a99c67eaeee4b0164e0d
Teammiracle25 <teammiracle25@gmail.com>
2023-12-13 10:17:55 +0530

    first commit

---
+++ src/components/Auth.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+// import './Auth.css';
+export default function (props) {
+  let [authMode, setAuthMode] = useState("signin")
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h1 className="Auth-form-title">Sign-Up</h1>
+          
+          <div className="form-group mt-3">
+            <label>First Name</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="e.g Your name"
+            /><br/><br/>
+            <label>Last Name</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="e.g Initial or Second Name"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          
+        </div>
+      </form>
+    </div>
+  )
+}
